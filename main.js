8bb63e22a998d0027ce3659ccc698e1156b2b305
juaprigal <juaprigal@outlook.es>
2020-02-27 18:58:07 +0100

    Subiendo ejercicio 3.2

---
+++ main.js
@@ -0,0 +1,20 @@
+/* 
+
+Escribe un programa que declare tres variables de nombre a, b y c, con valores de tipo number. A continuación:   
+
+1. Escribe una sentencia que muestre por pantalla la suma de las tres variables utilizando console.log
+2. Cambia el valor de la variable c.
+3. Escribe de nuevo una sentencia que muestre por pantalla la suma de las tres variables utilizando console.log
+
+*/
+
+
+const a = 5;
+const b = 4;
+let c = 6;
+
+console.log(a + b + c);
+
+c = 7
+
+console.log(a + b + c);
